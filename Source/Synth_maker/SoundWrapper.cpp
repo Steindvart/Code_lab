88324dfe170157f4a9276bb27ae7a183295d6873
Kalashnikov Ivan <steindvart.st@gmail.com>
2021-04-02 02:05:47 +0700

    Synth_maker: imlp SoundWrapper.

--- Source/Synth_maker/SoundWrapper.cpp
+++ Source/Synth_maker/SoundWrapper.cpp
@@ -1,48 +1,45 @@
+#define NOMINMAX
+
 #include "Shared/stdafx.h"
 
 #include "SoundWrapper.h"
 
 #include "Shared/Exception.h"
 
-#define BITS_PER_SAMPLE 8
+#define BITS_PER_BYTE CHAR_BIT 
 
-SoundWrapper::SoundWrapper(const std::wstring& outputDevice, int sampleRate, short channels, size_t blocks, int blockSamples)
+SoundWrapper::SoundWrapper(const std::wstring& outputDevice, int sampleRate, short channels, size_t blocks, size_t samplesPerBlock)
 	: m_sampleRate(sampleRate)
 	, m_channels(channels)
 	, m_blocksCount(blocks)
 	, m_blocksFree(blocks)
-	, m_blockSamples(blockSamples)
+	, m_samplesPerBlock(samplesPerBlock)
 	, m_blockCurrent(0)
-	, m_blocksData(m_blocksCount, std::vector<short>(m_blockSamples, 0))
+	, m_blocksData(m_blocksCount, std::vector<short>(m_samplesPerBlock, 0))
 	, m_waveHeaders(m_blocksCount)
 	, m_noizeFunc(nullptr)
+	, m_globalTime(0.0)
 {
 	WAVEFORMATEX waveFormat;
 	waveFormat.wFormatTag = WAVE_FORMAT_PCM;
 	waveFormat.nSamplesPerSec = m_sampleRate;
-	waveFormat.wBitsPerSample = sizeof(short) * BITS_PER_SAMPLE;
+	waveFormat.wBitsPerSample = sizeof(short) * BITS_PER_BYTE;
 	waveFormat.nChannels = m_channels;
-	waveFormat.nBlockAlign = (waveFormat.wBitsPerSample / BITS_PER_SAMPLE) * waveFormat.nChannels;
+	waveFormat.nBlockAlign = (waveFormat.wBitsPerSample / BITS_PER_BYTE) * waveFormat.nChannels;
 	waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
 	waveFormat.cbSize = 0;
 
-	int deviceID = GetDeviceId(outputDevice);
+	size_t deviceID = GetDeviceId(outputDevice);
 	if (deviceID == ERROR_BAD_DEVICE)
 		throw Shared::Exception(L"SoundWrapper::SoundWrapper: device " + outputDevice + L" is not valid.");
 
-	if (waveOutOpen(&m_device, deviceID, &waveFormat, (DWORD_PTR)waveOutProcWrap, (DWORD_PTR)this, CALLBACK_FUNCTION) != S_OK)
+	if (waveOutOpen(&m_device, static_cast<UINT>(deviceID), &waveFormat, (DWORD_PTR)waveOutProcWrap, (DWORD_PTR)this, CALLBACK_FUNCTION) != S_OK)
 		throw Shared::Exception(L"SoundWrapper::SoundWrapper: cannot open device " + outputDevice);
 
-	for (size_t n = 0; n < m_blocksCount; n++)
-	{
-		m_waveHeaders[n] = std::make_unique<WAVEHDR>();
-		m_waveHeaders[n]->dwBufferLength = m_blockSamples * sizeof(short);
-		m_waveHeaders[n]->lpData = (LPSTR)(m_blocksData[n].data());
-	}
+	linkHeaders();
 
 	m_isReady = true;
-
-	m_thread = std::thread(&MainThread, this);
+	m_thread = std::thread(&SoundWrapper::MainThread, this);
 
 	// Start the ball rolling
 	//std::unique_lock<std::mutex> lm(m_muxBlockNotZero);
@@ -55,6 +52,22 @@ SoundWrapper::~SoundWrapper()
 {
 	m_isReady = false;
 	m_thread.join();
+}
+
+void SoundWrapper::Stop()
+{
+	m_isReady = false;
+	m_thread.join();
+}
+
+double SoundWrapper::GetGlobalTime() const
+{
+	return m_globalTime;
+}
+
+void SoundWrapper::SetNoizeFunc(std::function<double(double)>& f)
+{
+	m_noizeFunc = f;
 }
 
 std::vector<std::wstring> SoundWrapper::GetAudioDevicesNames()
@@ -90,69 +103,84 @@ bool SoundWrapper::deviceIsValid(const std::wstring& deviceName) const
 		return false;
 }
 
-// Handler for soundcard request for more data
-void SoundWrapper::waveOutProc(HWAVEOUT hWaveOut, UINT uMsg, DWORD dwParam1, DWORD dwParam2)
+void SoundWrapper::linkHeaders()
 {
-	if (uMsg != WOM_DONE) 
+	for (size_t n = 0; n < m_blocksCount; n++)
+	{
+		m_waveHeaders[n] = std::make_unique<WAVEHDR>();
+		m_waveHeaders[n]->dwBufferLength = static_cast<DWORD>(m_samplesPerBlock * sizeof(short));
+		m_waveHeaders[n]->lpData = (LPSTR)(m_blocksData[n].data());
+	}
+}
+
+double SoundWrapper::clip(double sample, double max)
+{
+	if (sample >= 0.0)
+		return std::fmin(sample, max);
+	else
+		return std::fmax(sample, -max);
+}
+
+// Handler for soundcard request for more data
+void SoundWrapper::waveOutProc(HWAVEOUT /*waveOut*/, UINT msg, DWORD /*param1*/, DWORD /*param2*/)
+{
+	if (msg != WOM_DONE) 
 		return;
 
 	m_blocksFree++;
-	std::unique_lock<std::mutex> lm(m_muxBlockNotZero);
-	m_cvBlockNotZero.notify_one();
+	std::unique_lock<std::mutex> lm(m_muxNoFreeBlocks);
+	m_cvNoFreeBlocks.notify_one();
 }
 
-// Static wrapper for sound card handler
-void CALLBACK SoundWrapper::waveOutProcWrap(HWAVEOUT hWaveOut, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
+void CALLBACK SoundWrapper::waveOutProcWrap(HWAVEOUT waveOut, UINT msg, DWORD instance, DWORD param1, DWORD param2)
 {
-	((SoundWrapper*)dwInstance)->waveOutProc(hWaveOut, uMsg, dwParam1, dwParam2);
+	reinterpret_cast<SoundWrapper*>(instance)->waveOutProc(waveOut, msg, param1, param2);
 }
 
 void SoundWrapper::MainThread()
 {
-	//m_dGlobalTime = 0.0;
-	//double dTimeStep = 1.0 / (double)m_nSampleRate;
+	double timeStep = 1.0 / m_sampleRate;
 
-	//// Goofy hack to get maximum integer for a type at run-time
-	//T nMaxSample = (T)pow(2, (sizeof(T) * 8) - 1) - 1;
-	//double dMaxSample = (double)nMaxSample;
-	//T nPreviousSample = 0;
+	constexpr short maxSample = std::numeric_limits<short>::max();
+	constexpr double dMaxSample = static_cast<double>(maxSample);
+	short previousSample = 0;
 
-	//while (m_bReady)
-	//{
-	//	// Wait for block to become available
-	//	if (m_nBlockFree == 0)
-	//	{
-	//		unique_lock<mutex> lm(m_muxBlockNotZero);
-	//		m_cvBlockNotZero.wait(lm);
-	//	}
+	while (m_isReady)
+	{
+		// Wait for block to become available
+		if (m_blocksFree == 0)
+		{
+			std::unique_lock<std::mutex> lock(m_muxNoFreeBlocks);
+			m_cvNoFreeBlocks.wait(lock);
+		}
 
-	//	// Block is here, so use it
-	//	m_nBlockFree--;
+		// Block is here, so use it
+		m_blocksFree--;
 
-	//	// Prepare block for processing
-	//	if (m_pWaveHeaders[m_nBlockCurrent].dwFlags & WHDR_PREPARED)
-	//		waveOutUnprepareHeader(m_hwDevice, &m_pWaveHeaders[m_nBlockCurrent], sizeof(WAVEHDR));
+		//#TODO - to make functions for logical pieces
+		// Prepare block for processing
+		if (m_waveHeaders[m_blockCurrent]->dwFlags & WHDR_PREPARED)
+			waveOutUnprepareHeader(m_device, m_waveHeaders[m_blockCurrent].get(), sizeof(WAVEHDR));
 
-	//	T nNewSample = 0;
-	//	int nCurrentBlock = m_nBlockCurrent * m_nBlockSamples;
+		short newSample = 0;
+		//#TODO - use containers logic?
+		for (size_t n = 0; n < m_samplesPerBlock; n++)
+		{
+			// User Process
+			if (m_noizeFunc == nullptr)
+				newSample = (short)(clip(0.0, 1.0) * dMaxSample);
+			else
+				newSample = (short)(clip(m_noizeFunc(m_globalTime), 1.0) * dMaxSample);
 
-	//	for (unsigned int n = 0; n < m_nBlockSamples; n++)
-	//	{
-	//		// User Process
-	//		if (m_userFunction == nullptr)
-	//			nNewSample = (T)(clip(UserProcess(m_dGlobalTime), 1.0) * dMaxSample);
-	//		else
-	//			nNewSample = (T)(clip(m_userFunction(m_dGlobalTime), 1.0) * dMaxSample);
+			//#TODO - to think about this
+			m_blocksData[m_blockCurrent][n] = newSample;
+			previousSample = newSample;
+			m_globalTime = m_globalTime + timeStep;
+		}
 
-	//		m_pBlockMemory[nCurrentBlock + n] = nNewSample;
-	//		nPreviousSample = nNewSample;
-	//		m_dGlobalTime = m_dGlobalTime + dTimeStep;
-	//	}
-
-	//	// Send block to sound device
-	//	waveOutPrepareHeader(m_hwDevice, &m_pWaveHeaders[m_nBlockCurrent], sizeof(WAVEHDR));
-	//	waveOutWrite(m_hwDevice, &m_pWaveHeaders[m_nBlockCurrent], sizeof(WAVEHDR));
-	//	m_nBlockCurrent++;
-	//	m_nBlockCurrent %= m_nBlockCount;
-	//}
+		// Send block to sound device
+		waveOutPrepareHeader(m_device, m_waveHeaders[m_blockCurrent].get(), sizeof(WAVEHDR));
+		waveOutWrite(m_device, m_waveHeaders[m_blockCurrent].get(), sizeof(WAVEHDR));
+		m_blockCurrent = (m_blockCurrent + 1) % m_blocksCount;
+	}
 }
