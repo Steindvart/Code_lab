bbe099398c88f4c65856cb6fb705de817b1c1fca
Kalashnikov Ivan <steindvart.st@gmail.com>
2021-07-12 15:57:45 +0700

    Cs_practice: Hello, world!

---
+++ Source/Cs_practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
